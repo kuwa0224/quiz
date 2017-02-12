7dd2385945bfbad582b89d9caceede1b44620f5e
Takeshi Kuwahara <kuwahara.takeshi@dena.jp>
2017-02-12 11:26:56 +0900

    Update for recipe
    https://github.com/labstack/echo/blob/master/cookbook/google-app-engine/welcome.go

---
+++ src/quiz/quiz.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type (
+	Template struct {
+		templates *template.Template
+	}
+)
+
+func init() {
+
+	t := &Template{
+		templates: template.Must(template.ParseFiles("templates/top.html")),
+	}
+	e.Renderer = t
+
+	e.GET("/", top)
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func top(c echo.Context) error {
+	return c.Render(http.StatusOK, "top", "Takeshi")
+}
