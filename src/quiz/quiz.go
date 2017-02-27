739da50b1af9a3a9c46fd76e6003aad2d2340420
Takeshi Kuwahara <kuwahara.takeshi@dena.jp>
2017-02-28 02:02:40 +0900

    Change to gin from echo

--- src/quiz/quiz.go
+++ src/quiz/quiz.go
@@ -1,46 +1,28 @@
 package main
 
 import (
-	"html/template"
-	"io"
 	"net/http"
 
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
 
-	"github.com/labstack/echo"
-)
-
-type (
-	Template struct {
-		templates *template.Template
-	}
+	"gopkg.in/gin-gonic/gin.v1"
 )
 
 func init() {
+	router := gin.Default()
 
-	t := &Template{
-		templates: template.Must(template.ParseGlob("templates/*")),
-	}
-	e.Renderer = t
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", top)
 
-	e.GET("/", top)
+	http.Handle("/", router)
 }
 
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	err := t.templates.ExecuteTemplate(w, name, data)
-	if err != nil {
-		gaec := appengine.NewContext(c.Request())
-		log.Errorf(gaec, "failed to execute template: %v", err)
-	}
+func top(c *gin.Context) {
+	ctx := appengine.WithContext(c, c.Request)
+	log.Infof(ctx, "OK")
 
-	return err
-}
-
-func top(c echo.Context) error {
-	gaec := appengine.NewContext(c.Request())
-
-	log.Infof(gaec, "OK")
-
-	return c.Render(http.StatusOK, "top", "Takeshi")
+	c.HTML(http.StatusOK, "top.html", gin.H{
+		"name": "Takeshi",
+	})
 }
