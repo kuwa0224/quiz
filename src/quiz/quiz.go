48b58d977c99e11b82b9646405c348b3cc8df6d6
Takeshi Kuwahara <kuwahara.takeshi@dena.jp>
2017-03-04 22:16:28 +0900

    POSTできるように

--- src/quiz/quiz.go
+++ src/quiz/quiz.go
@@ -9,30 +9,62 @@ import (
 	"gopkg.in/gin-gonic/gin.v1"
 )
 
+type Answer struct {
+	Description string `json:"description" binding:"required"`
+	Correct     bool   `json:"correct" binding:"required"`
+}
+
+type Quiz struct {
+	Question   string   `json:"question" binding:"required"`
+	Answers    []Answer `json:"answers" binding:"required"`
+	AnswerNote string   `json:"answerNote" binding:"required"`
+}
+
 func init() {
 	router := gin.Default()
 
 	router.LoadHTMLFiles(
 		"templates/top.html",
 		"templates/admin/top.html",
+		"templates/admin/new.html",
 	)
 	// router.LoadHTMLGlob("templates/admin/top.html")
-	router.GET("/", top)
-	router.GET("/admin", admin)
+	router.GET("/", Top)
+	router.GET("/admin", QuizList)
+	router.GET("/admin/new", QuizNewForm)
+	router.POST("/admin/new", QuizNew)
 
 	http.Handle("/", router)
 }
 
-func top(c *gin.Context) {
+func Top(c *gin.Context) {
 	ctx := appengine.NewContext(c.Request)
 	log.Infof(ctx, "show top")
 
 	c.HTML(http.StatusOK, "top.html", gin.H{})
 }
 
-func admin(c *gin.Context) {
+func QuizList(c *gin.Context) {
 
 	// TODO 設定されたクイズの一覧を表示
 
 	c.HTML(http.StatusOK, "admin/top.html", gin.H{})
+}
+
+func QuizNewForm(c *gin.Context) {
+
+	c.HTML(http.StatusOK, "admin/new.html", gin.H{})
+}
+
+func QuizNew(c *gin.Context) {
+	ctx := appengine.NewContext(c.Request)
+
+	var quiz Quiz
+	if err := c.BindJSON(&quiz); err != nil {
+		log.Errorf(ctx, "Failed to bind : %v", err)
+		c.JSON(http.StatusBadRequest, nil)
+		return
+	}
+
+	c.JSON(http.StatusOK, nil)
 }
